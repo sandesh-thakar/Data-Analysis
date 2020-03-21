e9fe4c4648ef128d227e21ebe684c99775a7d21e
Sandesh Thakar <f20170181@hyderabad.bits-pilani.ac.in>
2020-03-21 22:28:17 +0530

    New folder for vizualization

--- IPL/dot_ball_analysis.py
+++ IPL/dot_ball_analysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 deliveries = pd.read_csv('deliveries.csv')
 matches = pd.read_csv('matches.csv')
 
-matches_2019 = matches[matches['season'] == 2019]
+matches_2019 = matches[matches['season'] >= 2018]
 
 mat_19 = []
 
@@ -27,6 +27,7 @@ for bowl in bowlers:
     bowlers_data[bowl] = dict()
     bowlers_data[bowl]['dots'] = 0
     bowlers_data[bowl]['balls'] = 0
+    bowlers_data[bowl]['wickets'] = 0
     
 for i in range(len(deliveries_2019_pp)):
     get_bowler = deliveries_2019_pp.iloc[i,8]
@@ -35,29 +36,38 @@ for i in range(len(deliveries_2019_pp)):
     if(get_bowler_runs == 0):
         bowlers_data[get_bowler]['dots'] += 1
     bowlers_data[get_bowler]['balls'] += 1
+    if(type(deliveries_2019_pp.iloc[i,18])==str):
+        bowlers_data[get_bowler]['wickets'] += 1
     
-bowlers_pp_dots = []
+bowlers_pp = []
 
 for bowl in bowlers:
-    if(bowlers_data[bowl]['dots']!=0):
-        bowlers_pp_dots.append([bowl,bowlers_data[bowl]['dots'],bowlers_data[bowl]['balls'], \
-                            bowlers_data[bowl]['balls']/bowlers_data[bowl]['dots']])
+    if(bowlers_data[bowl]['dots']!=0 and bowlers_data[bowl]['wickets']!=0):
+        bowlers_pp.append([bowl,bowlers_data[bowl]['wickets'],bowlers_data[bowl]['dots'], \
+                                bowlers_data[bowl]['balls'], \
+                            bowlers_data[bowl]['balls']/bowlers_data[bowl]['dots'],\
+                            bowlers_data[bowl]['balls']/bowlers_data[bowl]['wickets']])
     
-bowlers_pp_dots = pd.DataFrame(bowlers_pp_dots,columns=['bowler','dots', \
-                                                        'balls','balls_per_dot'])
+bowlers_pp = pd.DataFrame(bowlers_pp,columns=['bowler','wickets','dots', \
+                                                        'balls','balls_per_dot','sr'])
         
-bowlers_pp_dots = bowlers_pp_dots[bowlers_pp_dots['balls']>=60]
+bowlers_pp = bowlers_pp[bowlers_pp['balls']>=200]
     
-bowlers_pp_dots.sort_values("balls_per_dot", axis = 0, ascending = True, \
+bowlers_pp.sort_values('sr', axis = 0, ascending = True, \
                  inplace = True, na_position ='last') 
 
-x_data = [bowlers_pp_dots.iloc[i,3] for i in range(10,-1,-1)]
-y_data = [bowlers_pp_dots.iloc[i,0] for i in range(10,-1,-1)]
+x_data = [bowlers_pp.iloc[i,5] for i in range(10,-1,-1)]
+y_data = [bowlers_pp.iloc[i,0] for i in range(10,-1,-1)]
 y_pos = np.arange(len(y_data))
 
+colors = ['blue','red','yellow','dark blue','black','pink','yellow','violet','orange','blue','pink']
+
 # Create horizontal bars
-plt.barh(y_pos, x_data)
+plt.barh(y_pos, x_data,color=colors)
  
+
+plt.title("Powerplay SR since 2018 (min 200 balls)")
+
 # Create names on the y-axis
 plt.yticks(y_pos, y_data)
  
